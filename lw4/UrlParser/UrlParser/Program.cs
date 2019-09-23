f5fe9d998313265acedfe2788e09167505677bd6
NStepanova11 <n.stepanova11@yandex.ru>
2019-09-23 23:24:56 +0300

    async test

--- lw4/UrlParser/UrlParser/Program.cs
+++ lw4/UrlParser/UrlParser/Program.cs
@@ -7,9 +7,125 @@ using System.Net;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
+using System.Threading;
 
 namespace UrlParser
 {
+    class currency
+    {
+        public int Nominal { get; set; }
+        public string Name { get; set; }
+        public double Value { get; set; }
+    }
+
+    class Program
+    {
+        static List<string> typeList = new List<string>();
+        static string jsonString;
+        static async Task ReadValutesAcync()
+        {
+            using (StreamReader reader = new StreamReader("in.txt"))
+            {
+                string result;
+                while ((result = await reader.ReadLineAsync())!=null)
+                {
+                    typeList.Add(result);
+                    Console.WriteLine(result);
+                }
+            }
+            Console.WriteLine($"types reading completed");
+            //return typeList;
+        }
+
+        static async Task<List<currency>> GetJsonFromUrl()
+        {
+            string url = "https://www.cbr-xml-daily.ru/daily_json.js";
+            WebClient client = new WebClient();
+            client.Encoding = System.Text.Encoding.UTF8;
+            jsonString = await client.DownloadStringTaskAsync(new Uri(url));
+            //Console.WriteLine(jsonString);
+            List<currency> valuteList = new List<currency>();
+            valuteList = await Task.Run(()=>ParseJsonString());
+            Console.WriteLine($"json downloading completed");
+            return valuteList;
+        }
+
+        static List<currency> ParseJsonString()
+        {
+            JObject o = JsonConvert.DeserializeObject<JObject>(jsonString);
+            JObject valuteArray = o.Value<JObject>("Valute");
+
+            List<currency> currencyList = new List<currency>();
+
+            foreach (string valuteType in typeList)
+            {
+                JObject curType = valuteArray.Value<JObject>(valuteType);
+                currency cc = new currency();
+                cc.Nominal = curType.Value<int>("Nominal");
+                cc.Value = curType.Value<double>("Value");
+                cc.Name = curType.Value<string>("Name");
+                currencyList.Add(cc);
+            }
+            Console.WriteLine($"json parsing completed");
+            return currencyList;
+        }
+
+        static async Task SaveInfoAboutValutes()
+        {
+            List<currency> valutes = GetJsonFromUrl().Result;
+            using (StreamWriter sw = new StreamWriter("out.txt"))
+            {
+                foreach (currency valute in valutes)
+                {
+                    await sw.WriteLineAsync($"{valute.Nominal} {valute.Name} по курсу {valute.Value} руб.");
+                }
+            }
+            Console.WriteLine($"info saving completed");
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            ReadValutesAcync();
+            //GetJsonStringFromUrl();
+            SaveInfoAboutValutes();
+            Console.Read();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
     class currency
     {
         public int Nominal { get; set; }
@@ -63,7 +179,7 @@ namespace UrlParser
             OutputFileName = outputFileName;
         }
 
-        public List<string> ReadCurrencyTypes()
+        async Task<List<string>> ReadCurrencyTypes()
         {
             List<string> currencyTypeList = new List<string>();
 
@@ -93,27 +209,4 @@ namespace UrlParser
 
         }
     }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            UrlParser parser = new UrlParser();
-            List<string> valuteTypeList = new List<string>();
-            List<currency> valuteList = new List<currency>();
-
-            FileController fController = new FileController("in.txt", "out.txt");
-            valuteTypeList = fController.ReadCurrencyTypes();
-
-            valuteList = parser.ParseJsonString(valuteTypeList);
-
-            fController.SaveInfoAboutValutes(valuteList);
-
-            /*
-                Console.WriteLine(cc.Nominal.ToString());
-                Console.WriteLine(cc.Value.ToString());
-                Console.WriteLine(cc.Name.ToString());
-            */
-        }
-    }
-}
+    */
